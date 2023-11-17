2f8ad94e34ecdfa680adef5f96b780ec89de9932
Jack Gardner <shortfilmsbyjack@aol.com>
2023-11-16 21:42:04 -0800

    Added/modified autoSort
    Added get_file_type function to the autoSort file, and modified is_directory and get_file_extension

--- autoSort.py
+++ autoSort.py
@@ -2,22 +2,39 @@ import os
 import sys
 import shutil
 
-exten_to_path = {
+exten_to_type = {
     '.nes' : 'NES',
     '.snes' : 'SNES',
     '.n64' : 'N64',
     '.sfc' : 'SNES',
     '.nds' : 'NDS',
+    '.wbfs' : 'WII',
+    '.ciso' : 'GC',
+    '.gdi' : 'DC'
 }
 
-acceptable_extentions = ['.nes','.snes','.n64','.sfc','']
+acceptable_extentions = ['.nes','.snes','.n64','.sfc','.nds','.cue','.gdi']
 
 def is_directory(path):
-    return os.path.isdir(path)
+
+    directory_path = os.path.isdir(path)
+    return directory_path
 
 def get_file_extension(file_path):
+
     file_extension = os.path.splitext(file_path.lower())[1]
     return file_extension
+
+def get_file_type(file_path):
+
+    file_extension = get_file_extension(file_path)
+
+    if file_extension in exten_to_type.keys:
+        file_type = exten_to_type[file_extension]
+    else:
+        file_type = "UNKNOWN"
+        
+    return file_type
 
 def check_file_extension(file_path):
 
@@ -30,21 +47,30 @@ def check_file_extension(file_path):
         print(f"{file_path} is of UNKNOWN type")  
         return False
 
+def check_folder_type(folder_path):
+
+    if os.is_directory(folder_path):
+        for childfile in os.listdir(os.path.join(folder_path, childfile)):
+            if get_file_extension(childfile) in disk_extensions:   
+                print(f"{folder_path} is of DISK FOLDER type")
+                return True
+
+
 def compile_files(source_folder):
     output_list = []
 
     for filename in os.listdir(source_folder):
-        if check_file_extension(filename):
+        if check_file_extension(filename) or check_folder_type(filename):
             output_list.append(filename)
-    
-    return output_list
+
+    return output_list 
 
 def move_files_to_destination(source_files, source_folder, destination_folder):
     for filename in source_files:
 
         file_path = os.path.join(source_folder, filename)
 
-        ext_folder_path = os.path.join(destination_folder, exten_to_path[get_file_extension(file_path)])
+        ext_folder_path = os.path.join(destination_folder, exten_to_type[get_file_extension(file_path)])
 
         if not os.path.exists(ext_folder_path):
             os.makedirs(ext_folder_path)
